adde9251f54a8aa58b06b46176ec21408cf49baa
Aladya <alethiagmkjhgmail.com>
2026-09-20 19:47:41 -0300

    esqueleto controlador de reservas hecho

--- backend/src/reservas/reservas.controller.ts
+++ backend/src/reservas/reservas.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Post, Body, Param, Delete} from '@nestjs/common';
+import { ReservasService } from './reservas.service.js';
+import { CreateReservaDto } from './dto/create-reserva.dto.js';
 
-@Controller('reservas')
-export class ReservasController {}
+@Controller('reservas') //rutas /reservas
+export class ReservasController {
+  constructor(private readonly reservasService: ReservasService) {}
+//ruta crear reserva post
+  @Post()
+  crear(@Body() datosReserva: CreateReservaDto) {
+    return this.reservasService.crearReserva(datosReserva); 
+  }
+
+//ruta listar reservas
+  @Get()
+  listar() {
+    return "por completar: listado de los turnos" // this.reservasService.listarReserva();
+  }
+  //ruta cancelar reserva especfica
+  @Delete(':id')
+  cancelar(@Param('id') id: string) {
+    return "pendiente:cancelacion de turno" //this.reservasService.cancelarReserva(+id);
+  }
+}
