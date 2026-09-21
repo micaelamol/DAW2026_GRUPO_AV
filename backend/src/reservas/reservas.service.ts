cdd1af0ee64c9f7ae0947bf945ddcad09d9d418f
Aladya <alethiagmkjhgmail.com>
2026-09-20 21:41:15 -0300

    retoques a reservas.service y validacion de reservas duplicadas

--- backend/src/reservas/reservas.service.ts
+++ backend/src/reservas/reservas.service.ts
@@ -2,29 +2,32 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateReservaDto } from './dto/create-reserva.dto.js';
 import { ReservaEntity } from './entities/reserva.entity.js';
 import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EstadosReservas } from './enums/estados-reservas.enum.js';
 
 @Injectable()
 export class ReservasService {
     
-    constructor() {}
+    constructor(@InjectRepository(ReservaEntity)private reservaRepositorio: Repository<ReservaEntity>,) {}
 
     async crearReserva(dto: CreateReservaDto) { 
-        const fechaTurno = new Date(dto.fecha_hora);
-        this.validarFechaReserva(fechaTurno);
+        const fechaReserva = new Date(dto.fecha_hora);
+        this.validarFechaReserva(fechaReserva);
+        const reservaDuplicada= await this.reservaRepositorio.findOne({where:{ medico:{ id: dto.id_medico },fecha_hora: dto.fechaReserva,estado:EstadosReservas.ACTIVO}})
     }
 
-    private validarFechaReserva(fechaTurno: Date) {
+    private validarFechaReserva(fechaReserva: Date) {
 
-        const horaTurno = fechaTurno.getHours();
-        if (horaTurno < 8 || horaTurno > 15) {
+        const horaReserva = fechaReserva.getHours();
+        if (horaReserva < 8 || horaReserva > 15) {
             throw new BadRequestException("El horario de atención es de 8 a 16 hs y los turnos duran 1 hora.");
         }
-        const diaDeLaSemana = fechaTurno.getDay();
+        const diaDeLaSemana = fechaReserva.getDay();
         if (diaDeLaSemana === 0 || diaDeLaSemana === 6) {
             throw new BadRequestException("Solo se atienden reservas de Lunes a Viernes.");
         }
         const hoy = new Date();
-        const diferenciaMiliseg= fechaTurno.getTime() - hoy.getTime();
+        const diferenciaMiliseg= fechaReserva.getTime() - hoy.getTime();
         const diferenciaDias = diferenciaMiliseg / (1000 * 60 * 60 * 24);
 
         if (diferenciaDias < 0) {
